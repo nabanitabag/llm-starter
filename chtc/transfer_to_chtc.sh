b9026ff483a19d468ea0e96e9cf463b38f56a453
nbag <nbag@wisc.edu>
2026-04-01 13:02:01 -0500

    edit trasnfer sfript

--- chtc/transfer_to_chtc.sh
+++ chtc/transfer_to_chtc.sh
@@ -1,30 +1,46 @@
 #!/bin/bash
 source .env
 
-f=llm-starter
-
-cd ../.. # cd just outside the repo
-
-# 1. Create the tarball
-tar --exclude='.git' \
-    --exclude='.idea'  \
-    -czvf ${f}.tar.gz $f
-
+f=Verlog
 USER=${CHTC_USER}
 HOSTNAME="ap2001.chtc.wisc.edu"
 
+cd ../.. # cd just outside the repo
+
+# Establish persistent SSH connection (ask password once)
+echo "Establishing SSH connection..."
+ssh -o ControlMaster=auto -o ControlPath=~/.ssh/control-%r@%h:%p -o ControlPersist=10m -fN ${USER}@${HOSTNAME}
+
+# 1. Create the tarballs
+tar --exclude='.git' \
+    --exclude='.idea'  \
+    --no-xattrs \
+    -czvf ${f}.tar.gz $f
+
+# Also tar BALROG if it exists
+if [ -d "../../BALROG" ]; then
+    echo "Packaging BALROG..."
+    tar --exclude='.git' --no-xattrs -czvf BALROG.tar.gz -C ../.. BALROG
+fi
+
 echo "============================================"
-echo "1. Transferring tarball to CHTC Staging..."
+echo "1. Transferring tarballs to CHTC Staging..."
 echo "============================================"
-scp ${f}.tar.gz ${USER}@${HOSTNAME}:/staging/${USER}/
+scp -o ControlPath=~/.ssh/control-%r@%h:%p ${f}.tar.gz ${USER}@${HOSTNAME}:/staging/${USER}/
+if [ -f "BALROG.tar.gz" ]; then
+    scp -o ControlPath=~/.ssh/control-%r@%h:%p BALROG.tar.gz ${USER}@${HOSTNAME}:/staging/${USER}/
+fi
 
 echo "============================================"
 echo "2. Syncing chtc folder to CHTC Home directory..."
 echo "============================================"
-# This safely mirrors the chtc folder (including .env) to your CHTC home directory
-rsync -avz ${f}/chtc ${USER}@${HOSTNAME}:~/${f}/
+rsync -avz -e "ssh -o ControlPath=~/.ssh/control-%r@%h:%p" ${f}/chtc ${USER}@${HOSTNAME}:~/${f}/
 
-# Clean up local tarball
+# Clean up
 rm ${f}.tar.gz
+rm -f BALROG.tar.gz
 
-echo "Done! You can now run ./login_chtc.sh"
+# Close SSH connection
+ssh -O exit -o ControlPath=~/.ssh/control-%r@%h:%p ${USER}@${HOSTNAME} 2>/dev/null
+
+echo "Transferred at $(date '+%Y-%m-%d %H:%M:%S'). Done!"
